720952616801cbefcd564912069ad6f560a52f06
martiijn <94526855+martijnsark@users.noreply.github.com>
2022-09-19 10:38:29 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Martijn")
+    basic.showString("is")
+    basic.showNumber(19)
+    basic.showString("years old")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Martijn")
+    basic.showString("is")
+    basic.showNumber(19)
+    basic.showString("jaar oud")
+})
+basic.showString("Martijn")
+basic.forever(function () {
+	
+})
